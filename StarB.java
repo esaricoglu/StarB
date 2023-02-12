80c7e000ac7ef44bfe8b5f88c9efe771d21064b5
esaricoglu <84877085+esaricoglu@users.noreply.github.com>
2023-02-12 17:26:57 +0300

    Add files via upload

---
+++ StarB.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class StarB {
+    public static void main(String[] args) {
+        String[][] matrix = new String[7][4];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i == 0 || i == 3 || i == 6) {
+                    matrix[i][j] = " * ";
+                } else if (j == 0 || j == 3){
+                    matrix[i][j] = " * ";
+                } else {
+                    matrix[i][j] = "   ";
+                }
+            }
+        }
+        for(String[] row : matrix){
+            for(String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
